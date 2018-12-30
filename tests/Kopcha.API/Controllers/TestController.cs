2b791c2160d8851e142de429fb20780835f5e6d5
PeaceCwz <me@barisceviz.com>
2018-12-31 02:33:07 +0300

    Created API Test Project

---
+++ tests/Kopcha.API/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Kopcha.Attributes;
+using Kopcha.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kopcha.API.Controllers
+{
+    [ApiController]
+    [Route("api/tests")]
+    public class TestController : ControllerBase
+    {
+        // GET
+        [Captcha(CaptchaType = CaptchaType.Ip, Duration = 5, Threshold = 20)]
+        [HttpGet("test1")]
+        public IActionResult Get()
+        {
+            return Ok("Test Ok");
+        }
+    }
+}
